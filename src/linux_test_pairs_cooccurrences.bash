307a866e813d8d7b1d82d58c65c989328277d46b
sarmstr5 <git.sarmstr5@gmail.com>
2017-10-08 16:23:13 -0400

    testing 1st reducer

--- src/linux_test_pairs_cooccurrences.bash
+++ src/linux_test_pairs_cooccurrences.bash
@@ -1,1 +1,2 @@
-cat ../test_input/ratings_small_processed.csv | python average_by_attribute_mapper.py | sort -k1,1 | python test_reducer.py
+cat ../test_input/ratings_small_processed.csv | python map_pair_counts.py | sort -k1,1 | python reduce_pair_users.py
+#cat ../test_input/ratings_small_processed.csv | python map_pair_counts.py | sort -k1,1 | python test_reducer.py
