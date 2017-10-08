6a1efca6f62c15ba8068e089651dab181049a3b4
sarmstr5 <git.sarmstr5@gmail.com>
2017-10-08 16:30:33 -0400

    tested in bash, seems to work

--- src/reduce_pair_users.py
+++ src/reduce_pair_users.py
@@ -11,23 +11,30 @@ for line in sys.stdin:
     # can user list(itertools.chain.from_iterable(some_list))
     user, movies = line.split('\t', 1)
     try:
-        # lists are emited as string eg '[1,2,3]'
-        movie_ids = ast.literal_eval(movies)
-        # need additional check if working with combiners?
-
-    except ValueError:
-        continue
-
-    for movie_id in movie_ids:
+        # single value
+        movie_id = int(movies)
         movies_reviewed.append(movie_id)
 
+    # need additional check if working with combiners?
+    except ValueError:
+        # lists are emited as string eg '[1,2,3]'
+        try:
+            movie_ids = ast.literal_eval(movies)
+            for movie_id in movie_ids:
+                movies_reviewed.append(movie_id)
+        # other issue
+        except:
+            continue
+
+print(movies_reviewed)
 # aggregated all the movies reviewed by user u
 # now emit movie reviewed pairs
 # pairs should be sorted so pair(a,b) is never (b,a)
 movies_reviewed = sorted(movies_reviewed)
-for i in range(0, movies_reviewed):
+
+for i in range(0, len(movies_reviewed)):
     id_i = movies_reviewed[i]
     j_start = id_i + 1
-    for j in range(j_start, movies_reviewed):
+    for j in range(j_start, len(movies_reviewed)):
         id_j = movies_reviewed[j]
         print("({},{})\t{}".format(id_i, id_j, [1]))
