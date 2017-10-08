0e5bb2c41c8861561c862dba478ac992e1577306
sarmstr5 <git.sarmstr5@gmail.com>
2017-10-08 16:24:02 -0400

    tested in bash, seems to work, debugging reducers

--- src/map_pair_counts.py
+++ src/map_pair_counts.py
@@ -1,7 +1,11 @@
+#! /usr/bin/env python
 import sys
 
 for line in sys.stdin:
-    fields = line.split()
-    user_id = fields[0]
-    movie_id = list(fields[1])
-    print('{}\t{}'.format(user_id, movie_id))
+    fields = line.split(",")
+    try:
+        user_id = int(fields[0])
+        movie_id = int(fields[1])
+        print('{}\t{}'.format(user_id, movie_id))
+    except ValueError:
+        continue
