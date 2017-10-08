a981c085c7ded95713e3f67a52a6fc277852c17c
sarmstr5 <git.sarmstr5@gmail.com>
2017-10-08 16:30:59 -0400

    uses tiny for debugging

--- src/linux_test_pairs_cooccurrences.bash
+++ src/linux_test_pairs_cooccurrences.bash
@@ -1,2 +1,2 @@
-cat ../test_input/ratings_small_processed.csv | python map_pair_counts.py | sort -k1,1 | python reduce_pair_users.py
+cat ../test_input/ratings_tiny_processed.csv | python map_pair_counts.py | sort -k1,1 | python reduce_pair_users.py
 #cat ../test_input/ratings_small_processed.csv | python map_pair_counts.py | sort -k1,1 | python test_reducer.py
