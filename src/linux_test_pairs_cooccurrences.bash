8e5f089fa78931554ff176a3d084df4d1119aa21
sarmstr5 <git.sarmstr5@gmail.com>
2017-10-09 15:12:20 -0400

    updated to detect boundary changes in users

--- src/linux_test_pairs_cooccurrences.bash
+++ src/linux_test_pairs_cooccurrences.bash
@@ -1,1 +1,3 @@
-cat ../test_input/ratings_small_processed.csv | python map_pair_counts.py | sort -k1,1 | python reduce_pair_users.py | sort -k1,1 | python reduce_pair_cooccurrences.py
+#cat ../test_input/ratings_tiny_processed.csv | python map_pair_counts.py | sort -k1,1 | python reduce_pair_users.py | sort -k1,1 
+cat ../test_input/ratings_tiny_processed.csv | python map_pair_counts.py | sort -k1,1 | python reduce_pair_users.py | sort -k1,1 | python reduce_pair_cooccurrences.py
+#cat ../test_input/ratings_small_processed.csv | python map_pair_counts.py | sort -k1,1 | python reduce_pair_users.py | sort -k1,1 | python reduce_pair_cooccurrences.py
--- src/reduce_pair_users.py
+++ src/reduce_pair_users.py
@@ -4,6 +4,18 @@ import ast
 # import itertools
 
 movies_reviewed = []
+current_user = None
+last_user = None
+
+def emit_movies_reviewed(reviewed_movies):
+    sorted_movies = sorted(reviewed_movies)
+    for i in range(0, len(sorted_movies)):
+        id_i = sorted_movies[i]
+        j_start = id_i + 1
+        for j in range(j_start, len(sorted_movies)):
+            id_j = sorted_movies[j]
+            print("({},{})\t{}".format(id_i, id_j, [1]))
+
 #input from STDIN
 # userid \t movieids
 for line in sys.stdin:
@@ -12,28 +24,30 @@ for line in sys.stdin:
     user, movies = line.split('\t', 1)
     try:
         # single value
-        movie_id = int(movies)
-        movies_reviewed.append(movie_id)
+        movie_ids = [int(movies)]
+        current_user = int(user)
 
     # need additional check if working with combiners?
     except ValueError:
         # lists are emited as string eg '[1,2,3]'
         try:
             movie_ids = ast.literal_eval(movies)
-            for movie_id in movie_ids:
-                movies_reviewed.append(movie_id)
+
         # other issue
         except:
             continue
 
+    if current_user and (current_user != last_user):
+        emit_movies_reviewed(movies_reviewed)
+        movies_reviewed = []
+        for movie_id in movie_ids:
+            movies_reviewed.append(movie_id)
+
+    else:
+        for movie_id in movie_ids:
+            movies_reviewed.append(movie_id)
+    last_user = current_user
+
 # aggregated all the movies reviewed by user u
 # now emit movie reviewed pairs
 # pairs should be sorted so pair(a,b) is never (b,a)
-movies_reviewed = sorted(movies_reviewed)
-
-for i in range(0, len(movies_reviewed)):
-    id_i = movies_reviewed[i]
-    j_start = id_i + 1
-    for j in range(j_start, len(movies_reviewed)):
-        id_j = movies_reviewed[j]
-        print("({},{})\t{}".format(id_i, id_j, [1]))
