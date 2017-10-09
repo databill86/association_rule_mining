f65da735bedfa9ccc229fec4ccbe0e79bce3a43f
sarmstr5 <git.sarmstr5@gmail.com>
2017-10-09 14:39:11 -0400

    successful linux test, stripes map

--- src/map_stripes_counts.py
+++ src/map_stripes_counts.py
@@ -1,11 +1,18 @@
 #! /usr/bin/env python
 import sys
+from collections import defaultdict
+
+user_dict = defaultdict(list)
 
 for line in sys.stdin:
     fields = line.split(",")
     try:
         user_id = int(fields[0])
         movie_id = int(fields[1])
-        print('{}\t{}'.format(user_id, movie_id))
+        user_dict[user_id].append(movie_id)
+
     except ValueError:
         continue
+
+for user_id, movie_list in user_dict.items():
+    print('{}\t{}'.format(user_id, movie_list))
