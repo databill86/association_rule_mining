d73e58e9e8745e7b7231a7d0efe479661cfc27ab
sarmstr5 <git.sarmstr5@gmail.com>
2017-10-19 14:30:47 -0400

    setting up scripts

--- src/hadoop_pairs.sh
+++ src/hadoop_pairs.sh
@@ -1,4 +1,14 @@
-{ time hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input input/ratings_processed.csv -output output_pairs2 -file ~/hw2/src/map_pair_counts.py -mapper ~/hw2/src/map_pair_counts.py -file ~/hw2/src/reduce_pair_users.py -reducer ~/hw2/src/reduce_pair_users.py -file ~/hw2/src/reduce_pair_cooccurrences.py -reducer ~/hw2/src/reduce_pair_cooccurrences.py >hadoop_output.text 2>&1; } 2>> hadoop_pairs_time.txt
+#!/bin/bash
+# run hadoop pairs process or time it
+
+hadoop_input_fn=input/ratings_processed.csv
+hadoop_output_fn=output/output_pairs3
+bast_output_fn=pairs_hp_output.txt
+bash_time_output=pairs_hp_time.txt
+
+echo "#" $hadoop_input_fn >> $bash_time_output
+
+{ time hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input $hadoop_input_fn -output $hadoop_output_fn -file ~/hw2/src/map_pair_counts.py -mapper ~/hw2/src/map_pair_counts.py -file ~/hw2/src/reduce_pair_users.py -reducer ~/hw2/src/reduce_pair_users.py -file ~/hw2/src/reduce_pair_cooccurrences.py -reducer ~/hw2/src/reduce_pair_cooccurrences.py >$bash_output_fn 2>&1; } 2>> $bash_time_output
 
 #hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input input/ratings_processed.csv -output output_pairs2 -file ~/hw2/src/map_pair_counts.py -mapper ~/hw2/src/map_pair_counts.py -file ~/hw2/src/reduce_pair_users.py -reducer ~/hw2/src/reduce_pair_users.py -file ~/hw2/src/reduce_pair_cooccurrences.py -reducer ~/hw2/src/reduce_pair_cooccurrences.py
 
--- src/hadoop_stripes.sh
+++ src/hadoop_stripes.sh
@@ -1,4 +1,14 @@
-{ time hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input input/ratings_processed.csv -output output_stripes_8 -file ~/hw2/src/map_stripes_counts.py -mapper ~/hw2/src/map_stripes_counts.py -file ~/hw2/src/reduce_stripes_users.py -reducer ~/hw2/src/reduce_stripes_users.py -file ~/hw2/src/reduce_stripes_cooccurrences.py -reducer ~/hw2/src/reduce_stripes_cooccurrences.py >stripe_output.txt 2>&1; } 2>> time.txt
+#!/bin/bash
+# run hadoop stripes process or time it
+
+hadoop_input_fn=input/ratings_processed.csv
+hadoop_output_fn=output/output_stripes1
+bash_output_fn=stripes_hp_output.txt
+bash_time_output=stripes_hp_time.txt
+
+echo "#" $hadoop_input_fn >> $bash_time_output
+
+{ time hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input $hadoop_input_fn -output $hadoop_output_fn -file ~/hw2/src/map_stripes_counts.py -mapper ~/hw2/src/map_stripes_counts.py -file ~/hw2/src/reduce_stripes_users.py -reducer ~/hw2/src/reduce_stripes_users.py -file ~/hw2/src/reduce_stripes_cooccurrences.py -reducer ~/hw2/src/reduce_stripes_cooccurrences.py >$bash_output_fn 2>&1; } 2>> $bash_time_output
 
 #hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input input/ratings_processed.csv -output output_stripes_7 -file ~/hw2/src/map_stripes_counts.py -mapper ~/hw2/src/map_stripes_counts.py -file ~/hw2/src/reduce_stripes_users.py -reducer ~/hw2/src/reduce_stripes_users.py -file ~/hw2/src/reduce_stripes_cooccurrences.py -reducer ~/hw2/src/reduce_stripes_cooccurrences.py
 
