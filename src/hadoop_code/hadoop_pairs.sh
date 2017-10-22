45adfefff81a847ab1f5fa33353b139241efba74
sarmstr5 <git.sarmstr5@gmail.com>
2017-10-22 14:28:45 -0400

    updated pair script

--- src/hadoop_code/hadoop_pairs.sh
+++ src/hadoop_code/hadoop_pairs.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-# run hadoop pairs process or time it
+# run hadoop stripes process or time it
 
-input_fn=five_eighths.csv
-
-# input output dirs
+input_fn=sixteenth_file.csv
 hadoop_input_fn="input/$input_fn"
-hadoop_output_fn="pairs_output/$input_fn"
-bast_output_fn=pairs_hp_output.txt
-bash_time_output=pairs_hp_time.txt
 
-# python files
-mapper_fn=~/hw2/src/hadoop_code/map_pair_counts.py
-reducer_users_fn=~/hw2/src/hadoop_code/reduce_pair_users.py
-reducer_occurrences_fn=~/hw2/src/hadoop_code/reduce_pair_cooccurrences.py
+hadoop_output_fn=output_pairs/sixteenthagain
+bash_output_fn=pairs_hp_output.txt
+bash_time_output=pairs_hp_time.txt
 
 #wc
 local_path="/home/sarmstr5/hw2/data/truncated_files/$input_fn"
 file_wc=$(cat $local_path | wc -l)
 
+echo "#" $hadoop_input_fn >> $bash_time_output
 echo "#" $file_wc >> $bash_time_output
 
-{ time hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input $hadoop_input_fn -output $hadoop_output_fn -file $mapper_fn  -mapper $mapper_fn -file $reducer_users_fn -reducer $reducer_users_fn -file $reducer_occurrences_fn -reducer $reducer_occurrences_fn >$bash_output_fn 2>&1; } 2>> $bash_time_output
-echo "#" $hadoop_output_fn >> $bash_output_fn
+{ time hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input $hadoop_input_fn -output $hadoop_output_fn -file ~/hw2/src/hadoop_code/map_stripes_counts.py -mapper ~/hw2/src/hadoop_code/map_stripes_counts.py -file ~/hw2/src/hadoop_code/reduce_stripes_users.py -reducer ~/hw2/src/hadoop_code/reduce_stripes_users.py -file ~/hw2/src/hadoop_code/reduce_stripes_cooccurrences.py -reducer ~/hw2/src/hadoop_code/reduce_stripes_cooccurrences.py >$bash_output_fn 2>&1; } 2>> $bash_time_output
+
+#hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input input/ratings_processed.csv -output output_stripes_7 -file ~/hw2/src/map_stripes_counts.py -mapper ~/hw2/src/map_stripes_counts.py -file ~/hw2/src/reduce_stripes_users.py -reducer ~/hw2/src/reduce_stripes_users.py -file ~/hw2/src/reduce_stripes_cooccurrences.py -reducer ~/hw2/src/reduce_stripes_cooccurrences.py
+
+#hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input input/ratings.csv -output output -file ~/hw2/src/map_stripes_counts.py -mapper ~/hw2/src/map_stripe_counts.py file ~/hw2/src/reducer_stripess_users.py -reducer ~/hw2/src/reducer_pairs_cooccurrences.py
+
+
+#   28690615 2017-10-20 22:50 input/eighth_file.csv
+#   72917320 2017-10-21 20:17 input/five_eighths.csv
+#   65544659 2017-10-21 20:18 input/nine_sixs.csv
+#   58173094 2017-10-21 20:20 input/one_half.csv
+#   58201273 2017-10-20 22:50 input/quarter_file.csv
+#  119918080 2017-10-13 13:32 input/ratings_processed.csv
+#     552258 2017-10-19 12:43 input/ratings_small_processed.csv
+#   13933890 2017-10-20 22:50 input/sixteenth_file.csv
+#   87929620 2017-10-21 20:19 input/three_quarters.csv
---
+++ src/hadoop_code/need_to_fix_hp_pairs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# run hadoop pairs process or time it
+
+input_fn=five_eighths.csv
+
+# input output dirs
+hadoop_input_fn="input/$input_fn"
+hadoop_output_fn="pairs_output/$input_fn"
+bash_output_fn=pairs_hp_output.txt
+bash_time_output=pairs_hp_time.txt
+
+# python files
+mapper_fn=~/hw2/src/hadoop_code/map_pair_counts.py
+reducer_users_fn=~/hw2/src/hadoop_code/reduce_pair_users.py
+reducer_occurrences_fn=~/hw2/src/hadoop_code/reduce_pair_cooccurrences.py
+
+#wc
+local_path="/home/sarmstr5/hw2/data/truncated_files/$input_fn"
+file_wc=$(cat $local_path | wc -l)
+
+echo "#" $file_wc >> $bash_time_output
+
+{ time hadoop jar /apps/hadoop-2.4.1/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar -input $hadoop_input_fn -output $hadoop_output_fn -file $mapper_fn  -mapper $mapper_fn -file $reducer_users_fn -reducer $reducer_users_fn -file $reducer_occurrences_fn -reducer $reducer_occurrences_fn >$bash_output_fn 2>&1; } 2>> $bash_time_output
+
